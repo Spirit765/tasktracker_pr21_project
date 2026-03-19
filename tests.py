7f9ed5eae9e62496e96c247b4879772c7f493b2a
Student <dfefev@dfwdv>
2026-03-19 12:55:45 +0300

    Initial commit

---
+++ tests.py
@@ -0,0 +1,29 @@
+import pytest
+
+#load_data
+def test_load_data():
+    global user , tasks
+
+def test_save_users():
+    global user , tasks
+
+def test_save_tasks():
+    global tasks
+
+def test_register_tasks():
+    global tasks
+
+def test_login():
+    global user , tasks
+
+def test_add_task():
+    global user , tasks
+
+def test_delete_task():
+    global user , tasks
+
+def test_mark_done():
+    global user , tasks
+
+def test_refresh_tasks():
+    global user , tasks
