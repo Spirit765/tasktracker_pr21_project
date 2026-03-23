4208cce6ac5b93b377a95f766eab310ec7bb265a
Student <dfefev@dfwdv>
2026-03-23 13:37:09 +0300

    Initial commit

--- testtasktracker.py
+++ testtasktracker.py
@@ -2,28 +2,37 @@ import pytest
 
 #load_data
 def test_load_data():
+    assert True
     global user , tasks
 
 def test_save_users():
+    assert True
     global user , tasks
 
 def test_save_tasks():
+    assert True
     global tasks
 
 def test_register_tasks():
+    assert True
     global tasks
 
 def test_login():
+    assert True
     global user , tasks
 
 def test_add_task():
+    assert True
     global user , tasks
 
 def test_delete_task():
+    assert True
     global user , tasks
 
 def test_mark_done():
+    assert True
     global user , tasks
 
 def test_refresh_tasks():
+    assert True
     global user , tasks
